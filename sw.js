17d70af3436e8d1639ff017cc2abe74175221857
Steren <steren.giannini@gmail.com>
2024-12-14 17:41:20 -0800

    sw and manifest

---
+++ sw.js
@@ -0,0 +1,48 @@
+const cacheVersion = 'v1';
+const cacheName = 'sounds-' + cacheVersion;
+
+var urlsToCache = [
+  "/",
+  "sounds/cocotte.m4a",
+  "sounds/écureuil.m4a",
+  "sounds/encore.m4a",
+  "sounds/iaiao.m4a",
+  "sounds/maman.m4a",
+  "sounds/mamie.m4a",
+  "sounds/mathis.m4a",
+  "sounds/non.m4a",
+  "sounds/papi.m4a",
+  "sounds/papillon.m4a",
+  "sounds/poissons.m4a",
+  "sounds/rires.m4a",
+  "sounds/vache.m4a",
+  "sounds/voiture.m4a",
+  "sounds/wawawa.m4a",
+  "sounds/wow.m4a",
+  "sounds/yeah.m4a",
+  "sounds/banana.m4a",
+  "sounds/caca.m4a",
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        console.log('Caching sounds');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
